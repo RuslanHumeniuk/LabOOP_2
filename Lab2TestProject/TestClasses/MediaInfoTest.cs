f24febf5e4c0ce3815d095ab92457b6be6e3cd17
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-12 08:43:26 +0300

    Added 3 test method to MediaInfoTest

--- Lab2TestProject/TestClasses/MediaInfoTest.cs
+++ Lab2TestProject/TestClasses/MediaInfoTest.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using LabOp222.Models;
+using LabOp222.Models.MediaFiles;
+
 namespace Lab2TestProject.TestClasses
 {
     [TestClass]
     public class MediaInfoTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Title_WhenSetNotNullString_ReturnsTrue()
         {
+            MediaInfo instance = new Photo();
+            string newTitle = "TestTitle";
+
+            instance.Title = newTitle;
+
+            Assert.AreEqual(instance.Title, newTitle);            
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Title_WhenSetEmptyString_ReturnArgumentException()
+        {
+            MediaInfo instance = new Gallery();
+            instance.Title = String.Empty;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Title_WhenSetNull_ReturnsArgumentException()
+        {
+            MediaInfo instance = new Video() { Title = null };
+        }
+
     }
 }
